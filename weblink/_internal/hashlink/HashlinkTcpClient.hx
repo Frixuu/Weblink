a801e559efdcfa46ec7ba7c1f50defb7e270b9a8
Frixuu <kontakt@lukasz.xyz>
2024-09-21 22:27:12 +0200

    Separate TCP and Web server functionality
    This makes it easier to port Weblink to other platforms, such as Node.js, because it effectively reduces the needed API surface area.

---
+++ weblink/_internal/hashlink/HashlinkTcpClient.hx
@@ -0,0 +1,28 @@
+package weblink._internal.hashlink;
+
+#if hl
+import haxe.io.Bytes;
+import weblink._internal.TcpClient;
+import weblink._internal.hashlink.UvStreamHandle;
+
+final class HashlinkTcpClient extends TcpClient {
+	private var inner:UvStreamHandle;
+
+	public inline function new(handle:UvStreamHandle) {
+		this.inner = handle;
+	}
+
+	public function startReading(callback:(chunk:ReadChunk) -> Void):Void {
+		this.inner.readStart(callback);
+	}
+
+	public function writeBytes(bytes:Bytes) {
+		this.inner.writeBytes(bytes);
+	}
+
+	public function closeAsync(?callback:() -> Void) {
+		this.inner.closeAsync(callback);
+		@:nullSafety(Off) this.inner = null;
+	}
+}
+#end
